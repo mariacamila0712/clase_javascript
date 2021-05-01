3f6a86d7871be7a6f96956990a522662c8dc6bc8
Maria Camila <mariacmaza7@gmail.com>
2021-05-01 11:27:31 -0500

    Comentarios, sentencias, variables, constantes y uso de let

---
+++ script.js
@@ -0,0 +1,63 @@
+//Hola Mundo Alerta
+alert("Soy Javascript");
+
+//Comentarios
+
+//Comentario de linea
+/*Comentario de mas 
+de una linea*/
+
+//Sentencias
+
+//alert ('Hola'); alert('Mundo');
+
+//insercion automatica de punto y coma
+
+//alert('Hola')
+//alert('Mundo')
+
+//alert(3 + 1 + 2);
+
+//Variables
+let mensaje;
+mensaje = "Hola Mundo";
+alert(mensaje);
+
+let a = 5;
+let b = 6;
+let suma = a + b;
+console.log("El resultado de la suma es: ", suma);
+
+b = 7;
+console.log(b);
+
+let nombre = "Maria";
+let nombre2 = nombre;
+
+nombre = "Camila";
+console.log("El nombre uno es: ", nombre);
+console.log("El nombre dos es: ", nombre2);
+
+//Nombrar variables con camel case
+nombreUno = "Maria";
+nombreDos = "Camila";
+
+//Nombrar como underscore
+nombre_uno="Maria";
+nombre_dos ="Camila";
+
+//Nombres no comunes
+let $ = 1;
+let _ = 2;
+
+//No puedes nombrar variables
+/* let 1a = 3;
+let primer-nombre = 4; */
+
+"use strict"
+mensaje = "Hola Mundo";
+
+//Constantes
+const pi = 3.141624;
+const COLOR_ROJO = "#FFFFF";
+pi = 5;
